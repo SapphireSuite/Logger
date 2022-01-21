84877b24c46ac557a9efb3ccaed07f733dcf852e
mrouffet <maximerouffet@gmail.com>
2022-01-21 15:40:19 +0100

    [Add] Proto test

--- Tests/Prototype/main.cpp
+++ Tests/Prototype/main.cpp
@@ -4,8 +4,29 @@
 
 #define LOG(_str) std::cout << _str << std::endl;
 
+#include <SA/Logger/Logger.hpp>
+#include <SA/Logger/Streams/Console/ConsoleLogStream.hpp>
+using namespace Sa;
 
 int main()
 {
+	Logger logger;
+
+	ConsoleLogStream cslStream;
+
+	logger.Register(cslStream);
+
+	Log l{
+		L"MyFile",
+		16,
+		"MyFunc",
+		L"My Message!",
+		LogLevel::Infos,
+		L"MyChannel",
+		L"My Details"
+	};
+
+	logger.Output(l);
+
 	return 0;
 }
