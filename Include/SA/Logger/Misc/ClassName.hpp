b07d68b97c8a558b5a4e8d1248b74967514d510a
mrouffet <maximerouffet@gmail.com>
2022-02-10 21:06:39 +0100

    [Fix] Simple GetClassName

--- Include/SA/Logger/Misc/ClassName.hpp
+++ Include/SA/Logger/Misc/ClassName.hpp
@@ -8,15 +8,6 @@
 #include <string>
 #include <typeinfo>
 
-#include <SA/Support/Compilers.hpp>
-#include <SA/Support/Platforms.hpp>
-
-#if SA_UNIX && (SA_GNU || SA_CLANG)
-
-	#include <cxxabi.h>
-
-#endif
-
 namespace Sa
 {
 	/// \cond Internal
@@ -28,32 +19,11 @@ namespace Sa
 		{
 			(void)_in;
 
-			std::string result;
-
-		#if SA_UNIX && (SA_GNU || SA_CLANG)
-
-			// Query more precise name than typeid.
-
-			int status;
-
-			char* demangled = abi::__cxa_demangle(typeid(U).name(), 0, 0, &status);
-
-			if (status == 0)
-			{
-				result = demangled;
-				free(demangled);
-			}
-			else
-		
-		#else
-
-			result = typeid(U).name();
-
-		#endif
-
-			return result;
+			return typeid(U).name();
 		}
 	}
+
+	/// \endcond
 }
 
 #endif // GUARD
