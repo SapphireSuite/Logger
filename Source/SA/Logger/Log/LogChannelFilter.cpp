64096a3395f3b9ad2dcf938ea917a833faf97043
mrouffet <maximerouffet@gmail.com>
2022-05-05 22:06:49 +0900

    [Update] Channel more separators

--- Source/SA/Logger/Log/LogChannelFilter.cpp
+++ Source/SA/Logger/Log/LogChannelFilter.cpp
@@ -4,6 +4,8 @@
 
 namespace Sa
 {
+	static const std::wstring separators = L"./_-\\|";
+
 	bool LogChannelFilter::IsSingleChannelEnabled(const std::wstring& _chanName) const
 	{
 		auto it = mChannels.find(_chanName);
@@ -11,10 +13,9 @@ namespace Sa
 		return it != mChannels.cend() ? it->second : true;
 	}
 
-
 	bool LogChannelFilter::IsChannelEnabled(const std::wstring& _chanName, size_t _offset) const
 	{
-		const size_t fIndex = _chanName.find('/', _offset);
+		const size_t fIndex = _chanName.find_first_of(separators, _offset);
 
 		if (fIndex == std::wstring::npos)
 			return IsSingleChannelEnabled(_chanName);
