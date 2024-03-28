f140b571664b84d1912b94ffa1bb3af710da7a47
Maxime ROUFFET <maximerouffet@gmail.com>
2024-03-28 19:34:46 +0900

    [Split] LoggerBase/Logger/LoggerThread

--- Source/SA/Logger/Logger.cpp
+++ Source/SA/Logger/Logger.cpp
@@ -4,64 +4,6 @@
 
 namespace SA
 {
-	Logger::~Logger()
-	{
-		ClearStreams();
-	}
-
-	void Logger::Log(SA::Log _log)
-	{
-		ProcessLog(_log);
-	}
-
-//{ Streams
-
-	void Logger::ProcessLog(const SA::Log& _log, bool _bForce)
-	{
-		for (auto it = mStreams.begin(); it != mStreams.end(); ++it)
-			(*it)->ProcessLog(_log, _bForce);
-	}
-
-	void Logger::RegisterStream(ALogStream* _stream)
-	{
-		mStreams.push_back(_stream);
-	}
-
-	bool Logger::UnregisterStream(ALogStream* _stream)
-	{
-		for (auto it = mStreams.begin(); it != mStreams.end(); ++it)
-		{
-			if (*it == _stream)
-			{
-				mStreams.erase(it);
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	void Logger::ClearStreams(bool _bFlush)
-	{
-		for (auto it = mStreams.begin(); it != mStreams.end(); ++it)
-		{
-			if (_bFlush)
-				(*it)->Flush();
-			
-			delete *it;
-		}
-
-		mStreams.clear();
-	}
-
-	void Logger::Flush()
-	{
-		for (auto it = mStreams.begin(); it != mStreams.end(); ++it)
-			(*it)->Flush();
-	}
-
-//}
-
 //{ Frame Num
 
 	void Logger::IncrementFrameNum()
