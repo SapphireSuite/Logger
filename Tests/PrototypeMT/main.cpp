58c41696a38bcd524a5759c753ff182d082c6b50
mrouffet <maximerouffet@gmail.com>
2024-03-31 08:16:02 +0900

    [Add] ProtoMT submittedTime

--- Tests/PrototypeMT/main.cpp
+++ Tests/PrototypeMT/main.cpp
@@ -18,48 +18,53 @@ void LoggingThread()
 
 int main()
 {
-	std::chrono::time_point<std::chrono::system_clock> start, end;
+	std::chrono::time_point<std::chrono::system_clock> start, submitEnd, end;
 
-	//{
-	//{ Init
+//{ Init
 
-		SA::LoggerThread loggerThread(3000);
+	SA::LoggerThread loggerThread(4000);
 
-		loggerThread.CreateSteam<SA::ConsoleLogStream>();
-		loggerThread.CreateSteam<SA::FileLogStream>();
+	loggerThread.CreateSteam<SA::ConsoleLogStream>();
+	loggerThread.CreateSteam<SA::FileLogStream>();
 
-		SA::Debug::logger = &loggerThread;
+	SA::Debug::logger = &loggerThread;
 
-	//}
+//}
 
-		start = std::chrono::system_clock::now();
+	start = std::chrono::system_clock::now();
 
-		std::thread t1(LoggingThread);
-		std::thread t2(LoggingThread);
-		std::thread t3(LoggingThread);
-		std::thread t4(LoggingThread);
+	std::thread t1(LoggingThread);
+	std::thread t2(LoggingThread);
+	std::thread t3(LoggingThread);
+	std::thread t4(LoggingThread);
 
-		if (t1.joinable())
-			t1.join();
+	if (t1.joinable())
+		t1.join();
 
-		if (t2.joinable())
-			t2.join();
+	if (t2.joinable())
+		t2.join();
 
-		if (t3.joinable())
-			t3.join();
+	if (t3.joinable())
+		t3.join();
 
-		if (t4.joinable())
-			t4.join();
+	if (t4.joinable())
+		t4.join();
 
-		loggerThread.Flush();
-	//}
+	submitEnd = std::chrono::system_clock::now();
+
+	loggerThread.Flush();
 
 	end = std::chrono::system_clock::now();
 
-	auto time = end - start;
-	std::cout << "elapsed time: " <<
-		std::chrono::duration_cast<std::chrono::milliseconds>(time).count() << "ms / " <<
-		std::chrono::duration_cast<std::chrono::microseconds>(time).count() << "μs" << std::endl;
+	auto submitTime = submitEnd - start;
+	std::cout << "Total submitted time: " <<
+		std::chrono::duration_cast<std::chrono::milliseconds>(submitTime).count() << "ms / " <<
+		std::chrono::duration_cast<std::chrono::microseconds>(submitTime).count() << "μs" << std::endl;
+
+	auto totalTime = end - start;
+	std::cout << "Total elapsed time: " <<
+		std::chrono::duration_cast<std::chrono::milliseconds>(totalTime).count() << "ms / " <<
+		std::chrono::duration_cast<std::chrono::microseconds>(totalTime).count() << "μs" << std::endl;
 
 	return 0;
 }
