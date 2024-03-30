9cfc1a9fc3d6dbe12f9b65665aced360bbd8b0de
mrouffet <maximerouffet@gmail.com>
2024-03-30 12:41:23 +0900

    [Add] ProtoMT chrono

--- Tests/PrototypeMT/main.cpp
+++ Tests/PrototypeMT/main.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2023 Sapphire's Suite. All Rights Reserved.
 
 #include <iostream>
+#include <chrono>
 
 #include <SA/Collections/Debug>
 
@@ -17,24 +18,46 @@ void LoggingThread()
 
 int main()
 {
-	SA::Debug::InitDefaultLoggerThread();
+	std::chrono::time_point<std::chrono::system_clock> start, end;
 
-	std::thread t1(LoggingThread);
-	std::thread t2(LoggingThread);
-	std::thread t3(LoggingThread);
-	std::thread t4(LoggingThread);
+	{
+	//{ Init
 
-	if (t1.joinable())
-		t1.join();
+		SA::LoggerThread loggerThread;
 
-	if (t2.joinable())
-		t2.join();
+		loggerThread.CreateSteam<SA::ConsoleLogStream>();
+		loggerThread.CreateSteam<SA::FileLogStream>();
 
-	if (t3.joinable())
-		t3.join();
+		SA::Debug::logger = &loggerThread;
 
-	if (t4.joinable())
-		t4.join();
+	//}
+
+		start = std::chrono::system_clock::now();
+
+		std::thread t1(LoggingThread);
+		std::thread t2(LoggingThread);
+		std::thread t3(LoggingThread);
+		std::thread t4(LoggingThread);
+
+		if (t1.joinable())
+			t1.join();
+
+		if (t2.joinable())
+			t2.join();
+
+		if (t3.joinable())
+			t3.join();
+
+		if (t4.joinable())
+			t4.join();
+	}
+
+	end = std::chrono::system_clock::now();
+
+	auto time = end - start;
+	std::cout << "elapsed time: " <<
+		std::chrono::duration_cast<std::chrono::milliseconds>(time).count() << "ms / " <<
+		std::chrono::duration_cast<std::chrono::microseconds>(time).count() << "μs" << std::endl;
 
 	return 0;
 }
